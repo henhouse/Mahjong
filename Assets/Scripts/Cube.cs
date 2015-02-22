1cd27f1537fed2fb8a8cc75c95e07fea7a574974
Henry Henderson <henry925@gmail.com>
2015-02-22 18:30:53 -0500

    Implement basic selecting functionality.

---
+++ Assets/Scripts/Cube.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cube : MonoBehaviour
+{
+	public Game game;
+
+	void Start ()
+	{
+		game = GameObject.Find("Main Camera").GetComponent<Game>();
+	}
+
+	public void OnMouseDown()
+	{
+		renderer.materials[0].color = Color.green;
+		game.DoSomethingToClicked(this.gameObject);
+	}
+}
---
+++ Assets/Scripts/Game.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Game : MonoBehaviour {
+
+	public GameObject clickedGameObject;
+	
+	public void DoSomethingToClicked(GameObject clickedOn)
+	{
+		clickedGameObject = clickedOn;
+		print ("hi i ran.");
+	}
+}
