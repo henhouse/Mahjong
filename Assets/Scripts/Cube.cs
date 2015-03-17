b86f147dae1261fb2ee2f10d3d47d642e0abef23
Matt Geiger <violincounter@gmail.com>
2015-03-17 08:25:46 -0400

    Add collision detection for tiles

--- Assets/Scripts/Cube.cs
+++ Assets/Scripts/Cube.cs
@@ -35,13 +35,42 @@ public class Cube : MonoBehaviour
 			}
 			// Second clicked cube is different tag, destroy both
 			else {
-				Destroy(this.gameObject);
-				Destroy(selected.gameObject);
-				game.setSelected(null);
-				print ("Should destroy both");
+				// cast 4 rays in all directions. If there is >1 collision, do nothing.
+				if (this.twoSidesOpen() && selected.twoSidesOpen()) {
+					print ("Two sides open, should destroy both.");
+					Destroy(this.gameObject);
+					Destroy(selected.gameObject);
+				} else {
+					game.setSelected(null);
+					selected.toggleHighlight(false);
+					print ("Two sides not open.");
+				}
 			}
 		}
 	}
+
+	public bool twoSidesOpen() {
+		int numCollided = 0;
+		float rayDistance = .7f;
+
+		Vector3 up = transform.TransformDirection (Vector3.up);
+		Vector3 down = transform.TransformDirection (Vector3.down);
+		Vector3 left = transform.TransformDirection (Vector3.left);
+		Vector3 right = transform.TransformDirection (Vector3.right);
+
+		bool upCollided = Physics.Raycast (transform.position, up, rayDistance);
+		bool downCollided = Physics.Raycast (transform.position, down, rayDistance);
+		bool leftCollided = Physics.Raycast (transform.position, left, rayDistance);
+		bool rightCollided = Physics.Raycast (transform.position, right, rayDistance);
+
+		if (upCollided) numCollided++;
+		if (downCollided) numCollided++;
+		if (leftCollided) numCollided++;
+		if (rightCollided) numCollided++;
+		if (numCollided > 2) return false;
+		else return true;
+	}
+
 
 	private void toggleHighlight(bool toggleOn) {
 		Color c = renderer.material.color;
