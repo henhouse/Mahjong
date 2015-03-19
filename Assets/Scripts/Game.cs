99aaee5cbfea4e96c68e8ab569e7d5b707180291
Matt Geiger <violincounter@gmail.com>
2015-03-19 09:20:57 -0400

    Add you win text

--- Assets/Scripts/Game.cs
+++ Assets/Scripts/Game.cs
@@ -3,6 +3,13 @@ using System.Collections;
 
 public class Game : MonoBehaviour
 {
+
+	void Update() {
+		if (GameObject.Find("Cube") == null) {
+			//Instantiate(
+		}
+	}
+
 	public Cube clickedCube;
 
 	public void setSelected(Cube clickedOn)
