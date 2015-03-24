34ef8df0542ae66bc5e3a6915d1db3adf820972f
Henry Henderson <henry925@gmail.com>
2015-03-24 01:27:14 -0400

    No countdown, just time the game
    Still going too fast. Need to fix.

--- Assets/Scripts/Util.cs
+++ Assets/Scripts/Util.cs
@@ -3,30 +3,25 @@ using System.Collections;
 
 public class Util : MonoBehaviour {
 
-	public GUIText timeLeft;
+	public GUIText timeScore;
 	public GUIText matchesLeft;
 	public GUIText youWin;
 	public static int matches = 18;
-	public static float time = 45;
+	public static float time = 0;
 	bool won = false;
 
 	void Start()
 	{
 		youWin.enabled = false;
 	}
-	
+
 	// Update is called once per frame
 	void Update()
 	{
 		if (!won) {
-			time -= Time.deltaTime;
-			timeLeft.text = "Time Left: " + time;
-			if (time < 0)
-			{
-				Application.LoadLevel (0);
-				time = 30;
-				matches = 18;
-			}
+			time += Time.deltaTime;
+			timeScore.text = "Time: " + time;
+			//Application.LoadLevel (0);
 		}
 
 		matchesLeft.text = "Matches Left: " + matches;
